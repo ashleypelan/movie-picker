32a0c0989a183dd7a8849622d9d24bc9be2332e9
Ashley Pelan <ashley.pelan@gmail.com>
2015-06-28 12:45:05 -0600

    users can enter site and choose genre

--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,60 @@ var router = express.Router();
 var unirest = require('unirest');
 
 
-// function getRandomInt(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-//
-// function getRandomArrayValue(array) {
-//   var randomNumber = getRandomInt(0, array.count -1)
-//   var arrayValue = array[randomNumber]
-//   return arrayValue
-// }
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+function getRandomArrayValue(array) {
+  var randomNumber1 = getRandomInt(0, array.length -1);
+  var randomNumber2 = getRandomInt(0, array.length -1);
+  var arrayValue1 = array[randomNumber1];
+  var arrayValue2 = array[randomNumber2];
+  var resultArray = [arrayValue1, arrayValue2];
+  if (arrayValue1 === arrayValue2) {
+  return getRandomArrayValue(array);
+  }
+  return resultArray;
+}
+
+// for (var i = 0; i < movieGenres.length; i++) {
+//   var individualGenre = movieGenres[i];
+//
+// }
 
 router.get('/', function(req, res, next) {
-  unirest.get('https://api.themoviedb.org/3/movie/500?api_key=' + process.env.MOVIEKEY)
+  res.render('index');
+});
+
+
+router.get('/first', function(req, res, next) {
+  unirest.get('http://api.themoviedb.org/3/genre/movie/list?api_key=' + process.env.MOVIEKEY)
   .end(function (response) {
-    // var movieIDNumber = response.body.id
-    // console.log(movieIDNumber);
-    var movieIDNumber = response.body.title;
-    console.log(movieIDNumber);
+    var genres = response.body.genres;
 
-  res.render('index', { title: 'Express',
-                        info: movieIDNumber});
+    var result = getRandomArrayValue(genres);
+    console.log(result);
 
+
+
+
+  res.render('first', { title: 'Express',
+                        result1: result[0],
+                        result2: result[1]
+                        });
     });
 });
 
-router.get('/first', function(req, res, next) {
-  res.render('first');
+router.get('/second', function(req, res, next) {
+  unirest.get('http://api.themoviedb.org/3/genre/movie/list?api_key=' + process.env.MOVIEKEY)
+  .end(function (response) {
+
+  res.render('second');
+  });
 });
+
+
+
 
 module.exports = router;
 
